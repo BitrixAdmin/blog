c175f6cd4eebcf58d47dce5298425599b11743b7
RomIII <1633131@mail.ru>
2019-06-19 15:40:55 +0300

    from my local comp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!!!
+                    You are logged in!!! Trye git from local comp
                 </div>
             </div>
         </div>
